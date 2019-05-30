1e626651cf021be3d0398e47d375a1704552ce6c
Petya Buchkova <petya.buchkova@gmail.com>
2019-05-30 11:09:22 +0200

    Changed to my db credentials

--- database/script.sql
+++ database/script.sql
@@ -1,4 +1,7 @@
+\connect contentDB
+
 DROP TABLE IF EXISTS Books, Tags, Authors, BooksAuthors, MarkedToRead, BooksTags, BooksTagsTemp;
+
 CREATE TABLE Books
 (
   BookId serial NOT NULL,
@@ -15,52 +18,53 @@ CREATE TABLE Books
   PRIMARY KEY (BookId)
 );
 
-CREATE TABLE MarkedToRead(
+CREATE TABLE MarkedToRead
+(
   UserId INTEGER,
   BookId INTEGER REFERENCES Books(BookId) ON DELETE CASCADE,
   PRIMARY KEY (UserId, BookId)
 );
 
 
-CREATE TABLE Tags(
+CREATE TABLE Tags
+(
   TagId INTEGER,
   TagName TEXT,
   PRIMARY KEY (TagId)
 );
 
 
-CREATE TABLE BooksTagsTemp(
- GoodReadsBookId Integer,
- TagId Integer,
- Counter Integer
+CREATE TABLE BooksTagsTemp
+(
+  GoodReadsBookId Integer,
+  TagId Integer,
+  Counter Integer
 );
 
-CREATE TABLE BooksTags(
+CREATE TABLE BooksTags
+(
   GoodReadsBookId Integer REFERENCES Books(GoodReadsBookId) ON DELETE CASCADE,
   TagId Integer REFERENCES Tags(TagId) ON DELETE CASCADE,
   Counter INTEGER,
   PRIMARY KEY (TagId, GoodReadsBookId)
 );
 
-COPY books(GoodReadsBookId, Isbn, OriginalPublicationYear, OriginalTitle,
-InternationalTitle, LanguageCode, AverageRating,
-RatingsCount, ImageUrl, SmallImageUrl)
-FROM '/home/quenar/Desktop/DLS/Project/netbook/netbook-content/database/books_proc.csv' DELIMITER ',' CSV HEADER;
+\COPY books(GoodReadsBookId, Isbn, OriginalPublicationYear, OriginalTitle, InternationalTitle, LanguageCode, AverageRating, RatingsCount, ImageUrl, SmallImageUrl) FROM '/Users/petyabuchkova/Desktop/KEA/dls/netbook-content/database/books_proc.csv' DELIMITER ',' CSV HEADER;
 
 
-COPY MarkedToRead(userid, bookid) 
-FROM '/home/quenar/Desktop/DLS/Project/netbook/netbook-content/database/to_read.csv' DELIMITER ',' CSV HEADER;
+\COPY MarkedToRead(userid, bookid) FROM '/Users/petyabuchkova/Desktop/KEA/dls/netbook-content/database/to_read.csv' DELIMITER ',' CSV HEADER;
 
-COPY Tags(TagId, TagName)
-From '/home/quenar/Desktop/DLS/Project/netbook/netbook-content/database/tags.csv' DELIMITER ',' CSV HEADER;
+\COPY Tags(TagId, TagName) FROM '/Users/petyabuchkova/Desktop/KEA/dls/netbook-content/database/tags.csv' DELIMITER ',' CSV HEADER;
 
-COPY BooksTagsTemp(GoodReadsBookId, TagId, Counter)
-From '/home/quenar/Desktop/DLS/Project/netbook/netbook-content/database/bookstags.csv' DELIMITER ',' CSV HEADER;
+\COPY BooksTagsTemp(GoodReadsBookId, TagId, Counter) FROM '/Users/petyabuchkova/Desktop/KEA/dls/netbook-content/database/bookstags.csv' DELIMITER ',' CSV HEADER;
 
-INSERT INTO BooksTags SELECT DISTINCT ON (GoodReadsBookId, TagId) * FROM BooksTagsTemp;
+INSERT INTO BooksTags
+SELECT DISTINCT ON
+(GoodReadsBookId, TagId) * FROM BooksTagsTemp;
 DROP TABLE BooksTagsTemp;
 
-CREATE TABLE Authors(
+CREATE TABLE Authors
+(
   AuthorId serial NOT NULL,
   FullName TEXT,
   PRIMARY KEY (AuthorId)
@@ -68,15 +72,16 @@ CREATE TABLE Authors(
 
 CREATE TABLE BooksAuthors(
   BookId serial NOT NULL REFERENCES books(BookId) ON DELETE RESTRICT,
-  AuthorId INTEGER NOT NULL REFERENCES authors(AuthorId) ON DELETE NO ACTION,
-  PRIMARY KEY (BookId, AuthorId)
+  AuthorId INTEGER NOT NULL REFERENCES authors
+(AuthorId) ON
+DELETE NO ACTION,
+  PRIMARY KEY
+(BookId, AuthorId)
 );
 
-COPY authors(fullname)
-FROM '/home/quenar/Desktop/DLS/Project/netbook/netbook-content/database/authors.csv' DELIMITER ',' CSV HEADER;
+\COPY authors(fullname) FROM '/Users/petyabuchkova/Desktop/KEA/dls/netbook-content/database/authors.csv' DELIMITER ',' CSV HEADER;
 
-COPY booksauthors(bookid, authorid)
-FROM '/home/quenar/Desktop/DLS/Project/netbook/netbook-content/database/booksauthors.csv' DELIMITER ',' CSV HEADER;
+\COPY booksauthors(bookid, authorid) FROM '/Users/petyabuchkova/Desktop/KEA/dls/netbook-content/database/booksauthors.csv' DELIMITER ',' CSV HEADER;
 
 
 create extension unaccent;
